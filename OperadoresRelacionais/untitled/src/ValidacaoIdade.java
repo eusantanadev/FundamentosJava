6c61d9fdce7495c00fd764efd92b2bd6fba4ba6e
William Robson <William.santana.pe@gmail.com>
2026-08-11 20:58:30 -0300

    Operators
    Implementação de operadores

---
+++ OperadoresRelacionais/untitled/src/ValidacaoIdade.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ValidacaoIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int idade;
+        // Solicitação da idade ao usuário
+        System.out.print("Digite a sua idade: ");
+        idade = scanner.nextInt();
+        // Verificação da idade para determinar o acesso ao conteúdo
+        if (idade >= 18) {
+            System.out.println("Acesso concedido!");
+        } else {
+            System.out.println("Acesso negado.");
+
+        }
+    }
+}
