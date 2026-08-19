570ae404c0e4423184f2ad4301c693cf93cb435e
William Robson <William.santana.pe@gmail.com>
2026-08-18 21:40:46 -0300

    Update
    Update sistema de café

--- OperadoresRelacionais/untitled/src/SistemaPedidoCafe.java
+++ OperadoresRelacionais/untitled/src/SistemaPedidoCafe.java
@@ -1,52 +1,61 @@
 import java.util.Scanner;
-public class SistemaPedidoCafe{
+public class SistemaPedidoCafe {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bem vindo a Cafeteria do Will");
-        System.out.println("Escolha o tamanho do seu café: (P) Pequeno, (M) Médio, (G) Grande");
+        double valorTotal = 0;
+        int quantidadeCafes = 0;
+        boolean continuarPedido = true;
+        while (continuarPedido) {
+        System.out.println("Escolha o tamanho do seu café: (P) Pequeno, (M) Médio, (G) Grande: ");
         String tamanho = scanner.nextLine().toUpperCase();
-        System.out.println("Escolha o tipo do café: (E) Expresso, (C) Capuccino, (L) Latte");
-        String tipoCafe = scanner.nextLine().toUpperCase();
-
-        double preco = 0;
-
-        switch (tamanho){
-            case "P":
-                preco +=2.50;
-                break;
-            case "M":
-                preco += 3.00;
-                break;
-            case "G":
-                preco +=3.50;
-                break;
-            default:
-                System.out.println("Opção de tamanho invalida!");
-                return;
-
+        if (tamanho.equals("F")) {
+            continuarPedido = false;
+        } else {
+            System.out.println("Escolha o tipo do café: (E) Expresso, (C) Capuccino, (L) Latte: ");
+            String tipoCafe = scanner.nextLine().toUpperCase();
+            double preco = 0;
+            switch (tamanho) {
+                case "P":
+                    preco += 2.50;
+                    break;
+                case "M":
+                    preco += 3.00;
+                    break;
+                case "G":
+                    preco += 3.50;
+                    break;
+                default:
+                    System.out.println("Opção de tamanho invalida!");
+                    continue;
+            }
+            switch (tipoCafe) {
+                case "E":
+                    preco += 1.50;
+                    System.out.println("Café Expresso selecionando.");
+                    break;
+                case "C":
+                    preco += 2.00;
+                    System.out.println("Café Cappuccino selecionando.");
+                    break;
+                case "L":
+                    preco += 2.50;
+                    System.out.println("Café Latte selecionando.");
+                    break;
+                default:
+                    System.out.println("Opção do café invalido!");
+                    continue;
+                }
+                valorTotal += preco;
+                quantidadeCafes++;
+                System.out.println("Café adicionado ao pedido!");
+            }
         }
-
-        switch (tipoCafe){
-            case "E":
-                preco += 1.50;
-                System.out.println("Café Expresso selecionando.");
-                break;
-            case "C":
-                preco += 2.00;
-                System.out.println("Café Cappuccino selecionando.");
-                break;
-            case "L":
-                preco += 2.50;
-                System.out.println("Café Latte selecionando.");
-                break;
-            default:
-                System.out.println("Opção do café invalido!");
-                return;
-        }
-
-        System.out.println("Seu pedido foi confirmado!");
-        System.out.println("Total a pagar: R$" + preco);
+        System.out.println("Seu pedido foi finalizado. Total a pagar: R$" + valorTotal);
+        System.out.println("Quantidade de cafés pedidos: " + quantidadeCafes);
+        scanner.close();
     }
+
 }
