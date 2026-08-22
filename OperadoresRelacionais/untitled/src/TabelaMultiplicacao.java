580dc30fc35936a5c7fd549fd7a8ebaed75b7b60
William Robson <William.santana.pe@gmail.com>
2026-08-21 21:44:40 -0300

    Tabuada

--- OperadoresRelacionais/untitled/src/TabelaMultiplicacao.java
+++ OperadoresRelacionais/untitled/src/TabelaMultiplicacao.java
@@ -1,2 +1,12 @@
 public class TabelaMultiplicacao {
+    public static void main(String[] args){
+        System.out.println("Tabela Multiplicação");
+        for (int i = 1; i <=10; i++) {
+            System.out.println("\nTabuada do " + i + ":");
+            for (int j = 1; j <=10; j++) {
+                int resultado = i * j;
+                System.out.println(i + " X " + " = " + resultado);
+            }
+        }
+    }
 }
