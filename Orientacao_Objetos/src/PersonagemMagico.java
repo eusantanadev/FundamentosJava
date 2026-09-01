9a9b1485a4ee8e521b43911ab9528d62f6029a31
William Robson <William.santana.pe@gmail.com>
2026-09-01 09:01:30 -0300

    Construtor
    Adicionando um construtor

--- Orientacao_Objetos/src/PersonagemMagico.java
+++ Orientacao_Objetos/src/PersonagemMagico.java
@@ -1,6 +1,10 @@
 public class PersonagemMagico {
-
+        //Atributos
         String nome;
         String poderMagico;
         int nivelEnergia;
+        //Construtor exige a inclusão de um nome
+        public PersonagemMagico(String nome) {
+                this.nome = nome;
+        }
 }
